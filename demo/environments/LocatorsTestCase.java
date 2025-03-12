58023c4a250333a37cd4a27aed3ff17bebfe08c0
Nguyen Khanh Duy <nguyenkhanhduy.sgd@gmail.com>
2025-03-12 23:27:13 +0700

    fix bug add priority

--- demo/environments/LocatorsTestCase.java
+++ demo/environments/LocatorsTestCase.java
@@ -11,56 +11,58 @@ public class LocatorsTestCase {
     WebDriver driver;
 
     @BeforeClass
+    @Test(priority = 1)
     public void beforeClass() {
         driver = new ChromeDriver();
     }
 
-    @Test
+    @Test(priority = 2)
     public void TC_Get_URL() {
         driver.get("https://alada.vn/tai-khoan/dang-ky.html");
     }
 
-    @Test
+    @Test(priority = 3)
     public void TC_ID() {
         driver.findElement(By.id("txtFirstname"));
     }
 
-    @Test
+    @Test(priority = 4)
     public void TC_Class() {
-        driver.findElements(By.className("form-control"));
+        driver.findElement(By.className("form-control"));
     }
 
-    @Test
+    @Test(priority = 5)
     public void TC_Name() {
         driver.findElement(By.name("txtFirstname"));
     }
 
-    @Test
+    @Test(priority = 6)
     public void TC_TagName() {
         driver.findElements(By.tagName("input"));
     }
 
-    @Test
+    @Test(priority = 7)
     public void TC_Link_Text() {
         driver.findElements(By.linkText("Hướng dẫn sử dụng"));
     }
 
-    @Test
+    @Test(priority = 8)
     public void TC_Partial_Link_Text() {
         driver.findElement(By.partialLinkText("Giới thiệu"));
     }
 
-    @Test
+    @Test(priority = 9)
     public void TC_Css() {
-        driver.findElements(By.cssSelector("input[id='txtFirstname']"));
+        driver.findElement(By.cssSelector("input[id='txtFirstname']"));
     }
 
-    @Test
+    @Test(priority = 10)
     public void TC_XPath() {
         driver.findElement(By.xpath("//input[@id='txtFirstname']"));
     }
 
     @AfterClass
+    @Test(priority = 11)
     public void afterClass() {
         driver.quit();
     }
